cf91df50584461cc48ff889b7c79933069fe6702
VBergen <volker.bergen@tum.de>
2019-10-28 17:24:21 +0100

    collapsible off

--- conf.py
+++ conf.py
@@ -18,7 +18,7 @@ pygments_style = 'sphinx'
 # -- Options for HTML output ----------------------------------------------
 
 html_theme = 'sphinx_rtd_theme'
-html_theme_options = dict(navigation_depth=2)
+html_theme_options = dict(navigation_depth=2, titles_only=True)
 html_context = dict(display_github=True, github_user='theislab', github_repo='scvelo_notebooks',
                     github_version='master', conf_py_path='/')
 
